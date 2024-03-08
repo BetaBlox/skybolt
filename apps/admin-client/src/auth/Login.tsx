9de41a06c430dca1a4b6244808136102b9e0e58f
John Rake <john@betablox.com>
2024-03-08 16:38:08 -0600

    Change admin login form to look a bit different from app
    <!-- ps-id: edea3cb6-8402-425d-a612-fe4b38f5b1bd -->

--- apps/admin-client/src/auth/Login.tsx
+++ apps/admin-client/src/auth/Login.tsx
@@ -27,11 +27,11 @@ function Login() {
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <img
           className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          src="https://cdn-icons-png.flaticon.com/512/906/906343.png"
           alt="Your Company"
         />
         <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
+          Admin
         </h2>
       </div>
 
