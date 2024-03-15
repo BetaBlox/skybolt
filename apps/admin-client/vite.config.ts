c21b82c0220a09b48f55ade8c5e5c2c5c9288cfc
John Rake <john@betablox.com>
2024-03-14 21:34:29 -0500

    expose vite .env to client apps
    <!-- ps-id: 3e8993f8-df4e-4a54-980f-df0500993eaa -->

--- apps/admin-client/vite.config.ts
+++ apps/admin-client/vite.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8001', // local Nest app
-        changeOrigin: true,
+export default ({ mode }) => {
+  // Will load any .env vars prefix with VITE_
+  // see - https://vitejs.dev/guide/env-and-mode
+  const viteEnvVars = loadEnv(mode, resolve(__dirname, '../../'), 'VITE_');
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8081', // local Nest app
+          changeOrigin: true,
+        },
       },
+      host: 'localhost',
+      port: 8000,
+      strictPort: true,
+      open: true,
     },
-    host: 'localhost',
-    port: 8000,
-    strictPort: true,
-    open: true,
-  },
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
-  },
-});
+    define: {
+      'process.env': viteEnvVars,
+    },
+    resolve: {
+      alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+    },
+  });
+};
