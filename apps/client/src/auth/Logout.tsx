c679d8fe2c2024676d51a124caed033848b75c94
John Rake <john@betablox.com>
2024-03-17 14:04:44 -0500

    fullscreen logout
    <!-- ps-id: a8588516-1c86-423c-a217-40cfc4077c45 -->

--- apps/client/src/auth/Logout.tsx
+++ apps/client/src/auth/Logout.tsx
@@ -16,7 +16,7 @@ export default function Logout() {
   }, []);
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="full flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="text-center">
         <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Logging you out now.
