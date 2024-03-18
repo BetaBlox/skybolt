738d8d09cf7946558311f1814a57535c49e85ba8
John Rake <john@betablox.com>
2024-03-18 09:48:55 -0500

    <Avatar/>
    <!-- ps-id: ad49c754-8965-41bf-be5b-321d1217488f -->

--- apps/admin-client/src/layout/RootLayout.tsx
+++ apps/admin-client/src/layout/RootLayout.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { classNames } from '@repo/utils';
 import { HOME, LOGOUT, PROFILE } from '@/common/routes';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/Avatar';
 
 type NavItem = {
   name: string;
@@ -89,11 +90,13 @@ export default function RootLayout() {
                       <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span className="absolute -inset-1.5" />
                         <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
+                        <Avatar>
+                          <AvatarImage
+                            src="https://github.com/shadcn.png"
+                            alt="@shadcn"
+                          />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
                       </Menu.Button>
                     </div>
                     <Transition
