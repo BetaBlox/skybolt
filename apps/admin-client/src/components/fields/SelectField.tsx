a55b32f0f662ab56783558fe702ebbad21c03abe
John Rake <john@betablox.com>
2024-04-10 22:43:08 -0500

    text area and select
    <!-- ps-id: 7c6bb081-df23-4bf8-bb1d-9c8bbb4dc395 -->

--- apps/admin-client/src/components/fields/SelectField.tsx
+++ apps/admin-client/src/components/fields/SelectField.tsx
@@ -1,6 +1,12 @@
-import { ChangeEvent } from 'react';
 import FieldLabel from '../FieldLabel';
 import { AdminModelField, SelectOption } from '@repo/types';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/Select';
 
 interface Props {
   field: AdminModelField;
@@ -14,28 +20,27 @@ export default function SelectField({
   options = [],
   onChange,
 }: Props) {
-  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    onChange(field.name, e.currentTarget.value);
-  };
-
   return (
     <div>
       <FieldLabel field={field} />
-      <select
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-        id={field.name}
+      <Select
+        // id={field.name}
         name={field.name}
         value={value}
         required={field.isRequired}
-        onChange={handleChange}
+        onValueChange={(value: string) => onChange(field.name, value)}
       >
-        <option value="" />
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+        <SelectTrigger>
+          <SelectValue placeholder="Select one" />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </div>
   );
 }
--- apps/admin-client/src/components/fields/TextField.tsx
+++ apps/admin-client/src/components/fields/TextField.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent } from 'react';
-import FieldLabel from '../FieldLabel';
 import { AdminModelField } from '@repo/types';
+import FieldLabel from '@/components/FieldLabel';
+import { Textarea } from '@/components/Textarea';
 
 interface Props {
   field: AdminModelField;
@@ -15,11 +16,9 @@ export default function TextField({ field, value, onChange }: Props) {
   return (
     <div>
       <FieldLabel field={field} />
-      <textarea
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      <Textarea
         id={field.name}
         name={field.name}
-        rows={5}
         value={value || ''}
         required={field.isRequired}
         onChange={handleChange}
