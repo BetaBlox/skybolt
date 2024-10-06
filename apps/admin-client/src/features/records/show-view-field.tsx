6c28d82ec8ec5adbdf545b63aa248ffbf59fc3f2
John Rake <john@betablox.com>
2024-10-06 09:34:08 -0500

    refactor view field components
    <!-- ps-id: 3a53f072-0293-40d1-bbf2-e2bf453b85a0 -->

--- apps/admin-client/src/features/records/show-view-field.tsx
+++ apps/admin-client/src/features/records/show-view-field.tsx
@@ -1,9 +1,14 @@
+import { MODEL_RECORD } from '@/common/routes';
 import { getAttributeType, getDashboard } from '@repo/admin-config';
-import { AdminFieldType } from '@repo/types';
+import { AdminFieldType, AdminHasOneAttributeType } from '@repo/types';
+import { routeWithParams } from '@repo/utils';
 import { ExternalLinkIcon } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+type AdminRecord = Record<string, unknown> & { id: number };
 
 interface Props {
-  record: Record<string, unknown>;
+  record: AdminRecord;
   modelName: string;
   attribute: string;
 }
@@ -17,36 +22,77 @@ export default function ShowViewField({
   const value = record[attributeType.name];
 
   if (type === AdminFieldType.JSON) {
-    return JSON.stringify(value, null, 2);
+    return <JsonField value={value} />;
   } else if (type === AdminFieldType.BOOLEAN) {
-    return value === true ? 'yes' : 'no';
+    return <BooleanField value={value} />;
   } else if (type === AdminFieldType.RELATIONSHIP_HAS_ONE) {
-    // Should we even be using modelName here? Shouldn't we simply use the attributeType.name
-    const relationshipModelName = attributeType.modelName!;
-    const relationshipRecord =
-      record[relationshipModelName] || record[attributeType.name];
-
-    if (!relationshipRecord) {
-      throw new Error(
-        "No relationship record found on your model. Did you forget to 'include' it with your query?",
-      );
-    }
-
-    const relationshipDashboard = getDashboard(relationshipModelName);
-    return relationshipDashboard.getDisplayName(relationshipRecord);
+    return <HasOneField record={record} attributeType={attributeType} />;
   } else if (type === AdminFieldType.URL) {
-    if (!value) return null;
-    return (
-      <a
-        href={String(value)}
-        target="_blank"
-        className="flex flex-row items-center gap-x-2 underline"
-      >
-        <span className="line-clamp-1">{String(value)}</span>
-        <ExternalLinkIcon className="h-4 w-4" />
-      </a>
-    );
+    return <UrlField value={value} />;
   } else {
-    return String(record[attributeType.name]);
+    return <StringField value={value} />;
   }
 }
+
+const StringField = ({ value }: { value: unknown }) => {
+  return String(value);
+};
+
+const JsonField = ({ value }: { value: unknown }) => {
+  return <pre>{JSON.stringify(value, null, 2)}</pre>;
+};
+
+const BooleanField = ({ value }: { value: unknown }) => {
+  return value === true ? 'yes' : 'no';
+};
+
+const UrlField = ({ value }: { value: unknown }) => {
+  if (!value) return null;
+  return (
+    <a
+      href={String(value)}
+      target="_blank"
+      className="flex flex-row items-center gap-x-2 underline"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <span className="line-clamp-1">{String(value)}</span>
+      <ExternalLinkIcon className="h-4 w-4" />
+    </a>
+  );
+};
+
+const HasOneField = ({
+  record,
+  attributeType,
+}: {
+  record: AdminRecord;
+  attributeType: AdminHasOneAttributeType;
+}) => {
+  // Should we even be using modelName here? Shouldn't we simply use the attributeType.name
+  const relationshipModelName = attributeType.modelName;
+  const relationshipRecord = (record[relationshipModelName] ||
+    record[attributeType.name]) as AdminRecord;
+
+  if (!relationshipRecord) {
+    throw new Error(
+      "No relationship record found on your model. Did you forget to 'include' it with your query?",
+    );
+  }
+
+  const relationshipDashboard = getDashboard(relationshipModelName);
+  const recordDisplayName =
+    relationshipDashboard.getDisplayName(relationshipRecord);
+
+  return (
+    <Link
+      to={routeWithParams(MODEL_RECORD, {
+        modelName: relationshipModelName,
+        id: String(relationshipRecord.id),
+      })}
+      className="underline"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <span className="line-clamp-2">{String(recordDisplayName)}</span>
+    </Link>
+  );
+};
