a1d68089e84f2997aa9467fb71ba9405b2eaa3e1
John Rake <john@betablox.com>
2024-11-13 13:59:57 -0600

    delete existing impersonation tokens when creating
    <!-- ps-id: 2cb55b54-8e5a-4e6c-946b-79273323ba74 -->

--- apps/admin-api/src/auth/auth.service.ts
+++ apps/admin-api/src/auth/auth.service.ts
@@ -93,6 +93,12 @@ export class AuthService {
     const token = uuidv4();
     const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes from now
 
+    await this.prisma.impersonationToken.delete({
+      where: {
+        adminUserId,
+      },
+    });
+
     await this.prisma.impersonationToken.create({
       data: {
         token,
